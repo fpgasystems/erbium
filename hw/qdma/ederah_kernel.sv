ff2245d21a275911f0ed32c2c9a234b4073959fc
Fabio <fabio.maschi@inf.ethz.ch>
2020-02-14 11:12:37 +0100

    QDMA shell with double clock

--- hw/qdma/ederah_kernel.sv
+++ hw/qdma/ederah_kernel.sv
@@ -57,6 +57,19 @@ logic [32-1:0]                       ctrl_constant                  = 32'd1;
 //
 wire                                 inputs_stream_ttype;
 //
+// Pipe (AXI4-Stream host) interface results_stream
+wire                                      cdc_results_stream_tvalid;
+wire                                      cdc_results_stream_tready;
+wire [C_RESULTS_STREAM_TDATA_WIDTH-1:0]   cdc_results_stream_tdata ;
+wire [C_RESULTS_STREAM_TDATA_WIDTH/8-1:0] cdc_results_stream_tkeep ;
+wire                                      cdc_results_stream_tlast ;
+// Pipe (AXI4-Stream host) interface inputs_stream
+wire                                      cdc_inputs_stream_tvalid ;
+wire                                      cdc_inputs_stream_tready ;
+wire [C_INPUTS_STREAM_TDATA_WIDTH-1:0]    cdc_inputs_stream_tdata  ;
+wire                                      cdc_inputs_stream_tlast  ;
+wire                                      cdc_inputs_stream_ttype  ;
+//
 reg  [1:0]                           reader_state;
 reg  [1:0]                           nxt_reader_state;
 
@@ -145,24 +158,132 @@ ederah_wrapper #(
   .G_DATA_BUS_WIDTH         ( C_RESULTS_STREAM_TDATA_WIDTH )
 )
 inst_wrapper (
-  .clk_i                    ( data_clk              ), // kernel_clk
-  .rst_i                    ( data_rst_n            ), // kernel_rst_n
+  .clk_i                    ( kernel_clk            ),
+  .rst_i                    ( kernel_rst_n          ),
   // input
-  .rd_data_i                ( inputs_stream_tdata   ),
-  .rd_valid_i               ( inputs_stream_tvalid_krl ),
-  .rd_last_i                ( inputs_stream_tlast   ),
-  .rd_stype_i               ( inputs_stream_ttype   ),
-  .rd_ready_o               ( inputs_stream_tready  ),
+  .rd_data_i                ( cdc_inputs_stream_tdata  ),
+  .rd_valid_i               ( cdc_inputs_stream_tvalid ),
+  .rd_last_i                ( cdc_inputs_stream_tlast  ),
+  .rd_stype_i               ( cdc_inputs_stream_ttype  ),
+  .rd_ready_o               ( cdc_inputs_stream_tready ),
   // output
-  .wr_data_o                ( results_stream_tdata  ),
-  .wr_valid_o               ( results_stream_tvalid ),
-  .wr_last_o                ( results_stream_tlast  ),
-  .wr_ready_i               ( results_stream_tready )
+  .wr_data_o                ( cdc_results_stream_tdata  ),
+  .wr_valid_o               ( cdc_results_stream_tvalid ),
+  .wr_last_o                ( cdc_results_stream_tlast  ),
+  .wr_ready_i               ( cdc_results_stream_tready )
 );
 
-assign results_stream_tkeep = {C_INPUTS_STREAM_TDATA_WIDTH/8-1{1'b1}};
+assign cdc_results_stream_tkeep = {C_INPUTS_STREAM_TDATA_WIDTH/8-1{1'b1}};
 
-// inputs_stream_tkeep
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// CLOCK DOMAIN CROSSINGS                                                                         //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+xpm_fifo_axis #(
+  .CDC_SYNC_STAGES     ( 3                      ) , // DECIMAL
+  .CLOCKING_MODE       ( "independent_clock"    ) , // String
+  .ECC_MODE            ( "no_ecc"               ) , // String
+  .FIFO_DEPTH          ( 32                     ) , // DECIMAL
+  .FIFO_MEMORY_TYPE    ( "auto"                 ) , // String
+  .PACKET_FIFO         ( "false"                ) , // String
+  .PROG_EMPTY_THRESH   ( 5                      ) , // DECIMAL
+  .PROG_FULL_THRESH    ( 32-5                   ) , // DECIMAL
+  .RD_DATA_COUNT_WIDTH ( 6                      ) , // DECIMAL
+  .RELATED_CLOCKS      ( 0                      ) , // DECIMAL
+  .TDATA_WIDTH         ( C_INPUTS_STREAM_TDATA_WIDTH ) , // DECIMAL
+  .TDEST_WIDTH         ( 1                      ) , // DECIMAL
+  .TID_WIDTH           ( 1                      ) , // DECIMAL
+  .TUSER_WIDTH         ( 1                      ) , // DECIMAL
+  .USE_ADV_FEATURES    ( "1000"                 ) , // String
+  .WR_DATA_COUNT_WIDTH ( 6                      )   // DECIMAL
+)
+inst_xpm_fifo_axis_inputs (
+  .s_aclk             ( data_clk                 ) ,
+  .s_aresetn          ( data_rst_n               ) , // Keep tready low when not active
+  .s_axis_tvalid      ( inputs_stream_tvalid_krl ) ,
+  .s_axis_tready      ( inputs_stream_tready     ) ,
+  .s_axis_tdata       ( inputs_stream_tdata      ) ,
+  .s_axis_tstrb       ( 'b1                      ) ,
+  .s_axis_tkeep       ( inputs_stream_tkeep      ) ,
+  .s_axis_tlast       ( inputs_stream_tlast      ) ,
+  .s_axis_tid         ( 'b0                      ) ,
+  .s_axis_tdest       ( 'b0                      ) ,
+  .s_axis_tuser       ( inputs_stream_ttype      ) ,
+  .almost_full_axis   (                          ) ,
+  .prog_full_axis     (                          ) ,
+  .wr_data_count_axis (                          ) ,
+  .injectdbiterr_axis ( 1'b0                     ) ,
+  .injectsbiterr_axis ( 1'b0                     ) ,
+
+  .m_aclk             ( kernel_clk               ) ,
+  .m_axis_tvalid      ( cdc_inputs_stream_tvalid ) ,
+  .m_axis_tready      ( cdc_inputs_stream_tready ) ,
+  .m_axis_tdata       ( cdc_inputs_stream_tdata  ) ,
+  .m_axis_tstrb       (                          ) ,
+  .m_axis_tkeep       (                          ) ,
+  .m_axis_tlast       ( cdc_inputs_stream_tlast  ) ,
+  .m_axis_tid         (                          ) ,
+  .m_axis_tdest       (                          ) ,
+  .m_axis_tuser       ( cdc_inputs_stream_ttype  ) ,
+  .almost_empty_axis  (                          ) ,
+  .prog_empty_axis    (                          ) ,
+  .rd_data_count_axis (                          ) ,
+  .sbiterr_axis       (                          ) ,
+  .dbiterr_axis       (                          )
+);
+
+xpm_fifo_axis #(
+  .CDC_SYNC_STAGES     ( 3                      ) , // DECIMAL
+  .CLOCKING_MODE       ( "independent_clock"    ) , // String
+  .ECC_MODE            ( "no_ecc"               ) , // String
+  .FIFO_DEPTH          ( 32                     ) , // DECIMAL
+  .FIFO_MEMORY_TYPE    ( "auto"                 ) , // String
+  .PACKET_FIFO         ( "false"                ) , // String
+  .PROG_EMPTY_THRESH   ( 5                      ) , // DECIMAL
+  .PROG_FULL_THRESH    ( 32-5                   ) , // DECIMAL
+  .RD_DATA_COUNT_WIDTH ( 6                      ) , // DECIMAL
+  .RELATED_CLOCKS      ( 0                      ) , // DECIMAL
+  .TDATA_WIDTH         ( C_INPUTS_STREAM_TDATA_WIDTH ) , // DECIMAL
+  .TDEST_WIDTH         ( 1                      ) , // DECIMAL
+  .TID_WIDTH           ( 1                      ) , // DECIMAL
+  .TUSER_WIDTH         ( 1                      ) , // DECIMAL
+  .USE_ADV_FEATURES    ( "1000"                 ) , // String
+  .WR_DATA_COUNT_WIDTH ( 6                      )   // DECIMAL
+)
+inst_xpm_fifo_axis_results (
+  .s_aclk             ( kernel_clk                ) ,
+  .s_aresetn          ( kernel_rst_n              ) , // Keep tready low when not active
+  .s_axis_tvalid      ( cdc_results_stream_tvalid ) ,
+  .s_axis_tready      ( cdc_results_stream_tready ) ,
+  .s_axis_tdata       ( cdc_results_stream_tdata  ) ,
+  .s_axis_tstrb       ( 'b1                       ) ,
+  .s_axis_tkeep       ( cdc_results_stream_tkeep  ) ,
+  .s_axis_tlast       ( cdc_results_stream_tlast  ) ,
+  .s_axis_tid         ( 'b0                       ) ,
+  .s_axis_tdest       ( 'b0                       ) ,
+  .s_axis_tuser       ( 'b0                       ) ,
+  .almost_full_axis   (                           ) ,
+  .prog_full_axis     (                           ) ,
+  .wr_data_count_axis (                           ) ,
+  .injectdbiterr_axis ( 1'b0                      ) ,
+  .injectsbiterr_axis ( 1'b0                      ) ,
+
+  .m_aclk             ( data_clk                  ) ,
+  .m_axis_tvalid      ( results_stream_tvalid     ) ,
+  .m_axis_tready      ( results_stream_tready     ) ,
+  .m_axis_tdata       ( results_stream_tdata      ) ,
+  .m_axis_tstrb       (                           ) ,
+  .m_axis_tkeep       ( results_stream_tkeep      ) ,
+  .m_axis_tlast       ( results_stream_tlast      ) ,
+  .m_axis_tid         (                           ) ,
+  .m_axis_tdest       (                           ) ,
+  .m_axis_tuser       (                           ) ,
+  .almost_empty_axis  (                           ) ,
+  .prog_empty_axis    (                           ) ,
+  .rd_data_count_axis (                           ) ,
+  .sbiterr_axis       (                           ) ,
+  .dbiterr_axis       (                           )
+);
 
 endmodule : ederah_kernel
 `default_nettype wire
